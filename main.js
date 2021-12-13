1409ff768db8950ac633dd5f9b00af23b0b622f8
Gustavo valdez <valdezgustavo461@gmail.com>
2021-12-13 10:33:56 -0500

    adelantos de la pagina principal con boostrap

---
+++ main.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", function () {
+
+    var imagenes = [];
+    imagenes[0] = "img/foto1.jpg";
+    imagenes[1] = "img/foto2.jpg";
+    imagenes[2] = "img/foto3.jpg";
+    imagenes[3] = "img/foto4.jpg";
+    imagenes[4] = "img/foto5.jpg";
+
+    var indiceImagenes = 0;
+
+    function cambiarImagenes() {
+
+
+        document.slider.src = imagenes[indiceImagenes];
+        if (indiceImagenes < 2) {
+            indiceImagenes++;
+        } else {
+            indiceImagenes = 0;
+        }
+    }
+    this.setInterval(cambiarImagenes, 10000)
+});
